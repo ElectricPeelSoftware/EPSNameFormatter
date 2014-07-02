10144b8220cdf6c98c9b7d84c00248695665588e
Peter Stuart <peter@electricpeelsoftware.com>
2014-07-02 13:38:54 -0400

    Add missing AppDelegate file.

---
+++ Example/EPSNameFormatter/EPSAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  EPSAppDelegate.m
+//  EPSNameFormatter
+//
+//  Created by Peter Stuart on 7/2/14.
+//  Copyright (c) 2014 Electric Peel, LLC. All rights reserved.
+//
+
+#import "EPSAppDelegate.h"
+
+@implementation EPSAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    return YES;
+}
+
+@end
